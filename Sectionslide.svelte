f9d344f19035e730f098f458a8646501a597db58
VikasDrall <drallv960@gmail.com>
2024-06-19 21:45:09 +0530

    Section

---
+++ Sectionslide.svelte
@@ -0,0 +1,368 @@
+<script>
+
+</script>
+
+<div>
+    <div class="Slidebox">
+        <div class="Slidebox_full">
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200"  alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="Slidebox_box">
+                <div class="Slidebox_boximg">
+                    <img src="https://picsum.photos/id/237/200" alt="" />
+                </div>
+                <div class="Slidebox_boxcontent">
+                    <h6 class="mt-2">text1</h6>
+                    <h6 class="mt-1">text2</h6>
+                    <div>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star checked"></span>
+                        <span class="fa fa-star"></span>
+                        <span class="fa fa-star"></span>
+                    </div>
+                    <div class="mt-1 px-1 d-flex justify-content-between mb-2">
+                        <div>
+                            <p class="my-2">200</p>
+                        </div>
+                        <button type="button" class="btn btn-outline-success">+ Add</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+/* width */
+::-webkit-scrollbar {
+            width: 0px;
+            height: 0px;
+        }
+
+        /* Track */
+        ::-webkit-scrollbar-track {
+            background: #cccccc;
+        }
+
+        /* Handle */
+        ::-webkit-scrollbar-thumb {
+            background: #f2f2f2;
+        }
+
+        /* Handle on hover */
+        ::-webkit-scrollbar-thumb:hover {
+            background: #f2f2f2;
+        }
+
+        .checked {
+            color: #f7ca00;
+        }
+
+        .Slidebox {
+            width: 96%;
+            margin: auto;
+        }
+
+        .Slidebox_full {
+            overflow-x: scroll;
+            overflow-y: hidden;
+            white-space: nowrap;
+        }
+
+        .Slidebox_box {
+            width: 250px;
+            display: flex;
+            flex-direction: column;
+            margin: 10px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            display: inline-block;
+        }
+        .Slidebox_box:hover{
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+            transition: 0.6s;
+        }
+
+        .Slidebox_boximg {
+            width: 100%;
+            height: 200px;
+            overflow: hidden;
+        }
+
+        .Slidebox_boximg img {
+            width: 100%;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            object-position: center;
+            object-fit: cover;
+        }
+
+        .Slidebox_boxcontent {
+            display: flex;
+            flex-direction: column;
+            width: 94%;
+            margin: auto;
+        }
+        @media all and (max-width: 800px) {
+            .Slidebox_box {
+            width: 200px;
+            margin: 6px;
+        }
+        .Slidebox_boximg {
+            height: 150px;
+        } 
+        .Slidebox_box h6{
+            font-size: 14px;
+            margin-top: 10px;
+            line-height: 14px;
+        }
+        .Slidebox_box h4{
+            font-size: 13px;
+            line-height: 14px;
+        }
+        }
+        @media all and (max-width: 600px) {
+            .Slidebox_box {
+            width: 100px;
+            margin: 2px;
+        } 
+        .Slidebox_boximg {
+            height: 100px;
+        } 
+        .Slidebox_box h6{
+            font-size: 14px;
+            margin-top: 10px;
+            line-height: 14px;
+        }
+        .Slidebox_box h4{
+            font-size: 13px;
+            line-height: 14px;
+        }
+        .Slidebox_box button{
+            font-size: 10px;
+            padding: 4px;
+        }
+        .Slidebox_box p{
+            font-size: 12px;
+        }
+        }
+</style>
